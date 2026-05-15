ebc7ac8d7fd986bede892f37b919668bb1024b5f
Marinelle Joan U. Tambolero <mutambolero@up.edu.ph>
2026-05-15 12:07:25 +0800

    completed

--- portfolio_website/script.js
+++ portfolio_website/script.js
@@ -8,3 +8,18 @@ const observer = new IntersectionObserver((entries) => {
 }, { threshold: 0.1 });
 
 document.querySelectorAll(".fade-up").forEach((el) => observer.observe(el));
+
+const slideshows = document.querySelectorAll(".slideshow");
+
+slideshows.forEach((slideshow) => {
+  const slides = slideshow.querySelectorAll(".slide");
+  let currentSlide = 0;
+
+  setInterval(() => {
+    slides[currentSlide].classList.remove("active");
+
+    currentSlide = (currentSlide + 1) % slides.length;
+
+    slides[currentSlide].classList.add("active");
+  }, 2500);
+});
